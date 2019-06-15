2e00754df0d3030984eb4f03e8a3ec5029f85c66
gonza <gonzalo.christobal@gmail.com>
2019-06-15 19:24:17 -0400

    transition added

--- src/App.js
+++ src/App.js
@@ -2,26 +2,71 @@ import React, { useState } from "react";
 import "./base.css";
 import introImg from "./assets/1.jpg";
 import runes from "runes";
-import { animated, useTrail } from "react-spring";
+import { animated, useTrail, useSpring } from "react-spring";
 
 function App() {
-  const [toggle, set] = useState(true);
+  const [toggle, setToggle] = useState(false);
   const letters = runes("Привет всем");
   const config = { mass: 5, tension: 2000, friction: 150 };
 
+  const props = useSpring({
+    to: {
+      transform: toggle ? "translate3d(0,-100%,0)" : "translate3d(0,0,0)"
+    },
+    from: { transform: "translate3d(0,-100%,0)" }
+  });
+
+  const reverseProps = useSpring({
+    to: {
+      transform: toggle
+        ? "translate3d(0,100%,0) rotate3d(0,0,1,-35deg)"
+        : "translate3d(0,0,0) rotate3d(0,0,1,-35deg)"
+    },
+    from: { transform: "translate3d(0,100%,0) rotate3d(0,0,1,-35deg)" }
+  });
+
   const trail = useTrail(letters.length, {
     config,
-    onRest: () => set(true),
-    opacity: toggle ? 1 : 0,
-    x: toggle ? 0 : 20,
+    opacity: 1,
+    x: 0,
     from: { opacity: 0, x: 20, height: 0 }
   });
+
+  const boundingBox = (w, h, angle) => [
+    w * Math.abs(Math.cos(angle * (Math.PI / 180))) +
+      h * Math.abs(Math.sin(angle * (Math.PI / 180))),
+    w * Math.abs(Math.sin(angle * (Math.PI / 180))) +
+      h * Math.abs(Math.cos(angle * (Math.PI / 180)))
+  ];
+
+  const [boundingW, boundingH] = boundingBox(
+    window.innerWidth,
+    window.innerHeight,
+    35
+  );
 
   return (
     <main>
       <div className="content content--grid content--second">
-        <div className="content content--first">
-          <div className="content__move">
+        <h2 className="content__title">второй</h2>
+        <button
+          className="intro__enter content__leave"
+          onClick={() => setToggle(state => !state)}
+        >
+          leave
+        </button>
+      </div>
+      <animated.div
+        style={{ transform: "rotate3d(0,0,1,35deg)" }}
+        className={`content content--first ${
+          toggle ? "content__hidden" : null
+        }`}
+      >
+        <animated.div
+          style={{ ...props, width: boundingW, height: boundingH }}
+          className="content__move"
+        >
+          <animated.div style={{ ...reverseProps }} className="content__reverse">
             <div className="intro">
               <div
                 className="intro__img"
@@ -42,14 +87,14 @@ function App() {
               </h1>
               <button
                 className="intro__enter"
-                onMouseEnter={() => set(state => !state)}
+                onClick={() => setToggle(state => !state)}
               >
                 enter
               </button>
             </div>
-          </div>
-        </div>
-      </div>
+          </animated.div>
+        </animated.div>
+      </animated.div>
     </main>
   );
 }
